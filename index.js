4e4c42c3dd09b86789e5239a3e60aeddeb08c829
Abu Jubaer <abujubaer90@gmail.com>
2022-05-25 00:07:21 +0600

    initialize

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttyvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("tool_store");
+        const jewelry = database.collection("tool");
+
+        app.get("/tool/", async (req, res) => {
+            const cursor = await jewelry.find({}).toArray();
+            res.send(cursor);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, (req, res) => {
+    console.log("Running with port: " + port);
+})
